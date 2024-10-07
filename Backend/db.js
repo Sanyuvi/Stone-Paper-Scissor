a3c07150144329e868934141c197fd74f0181099
Santhosh <130633089+Sanyuvi@users.noreply.github.com>
2024-10-08 01:55:12 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,37 @@
 const { Pool } = require("pg");
 
 // PostgreSQL connection setup
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "stonepaperscissors",
+//   password: "test",
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
+require('dotenv').config();
+// const { Pool } = require('pg');
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "StonePaperScissors",
-  password: "test",
-  port: 5432,
+    connectionString: process.env.POSTGRES_URL,
 });
 
-module.exports = pool;
+// Example of a query to test the connection
+const testConnection = async () => {
+    try {
+        const client = await pool.connect();
+        console.log("Connected to the database!");
+        const res = await client.query('SELECT NOW()');
+        console.log('Current time:', res.rows[0]);
+        client.release();
+    } catch (err) {
+        console.error('Database connection error:', err);
+    }
+};
+
+testConnection();
+
+module.exports = pool
+
