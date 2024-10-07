164d8db9a4f5ef5b13d4363d3e232c8e4de84d6d
Santhosh <130633089+Sanyuvi@users.noreply.github.com>
2024-10-08 01:56:37 +0530

    Update GamePage.js

--- Frontend/spsgamefrontend/src/Components/GamePage.js
+++ Frontend/spsgamefrontend/src/Components/GamePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -7,11 +7,13 @@ const GamePage = () => {
   const [computerChoice, setComputerChoice] = useState(null);
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
-  const [rounds, setRounds] = useState([]);
-  const [gameId, setGameId] = useState(`game_${Date.now()}`);
-  const [currentRound, setCurrentRound] = useState(0);
+  const [rounds, setRounds] = useState(1);
+  const [userChoice, setUserChoice] = useState(false); 
   const [finalWinner, setFinalWinner] = useState(null);
+  const [startGame, setStartGame] = useState(true);
+  const [reGame, setReGame] = useState(false);
   const navigate = useNavigate();
+
 
   const handleGameHistoryClick = () => {
     navigate("/historyPage"); // Specify the path for the game history page
@@ -19,72 +21,160 @@ const GamePage = () => {
 
   const API = "http://localhost:5578";
   const icons = [
-    { name: "Stone", iconClass: "fa-solid fa-bomb", color: "text-muted" },
+    { id: 1, name: "Stone", iconClass: "fa-solid fa-bomb", color: "text-muted" },
     {
+      id: 2,
       name: "Paper",
       iconClass: "fa-regular fa-note-sticky",
       color: "text-success",
     },
     {
+      id: 3,
       name: "Scissors",
       iconClass: "fa-solid fa-scissors",
       color: "text-danger",
     },
   ];
 
-  const selectedChoice = (choicesIndex) => {
-    setPlayerChoice(choicesIndex);
-  };
+  const selectedChoice = async (choicesID) => {
+    const gameId = sessionStorage.getItem("gameId");
+    setRounds(rounds + 1);  
+    
+    const computerDetail = generateComputerChoice();
+    const  choiceDetails = icons.filter((data)=> data.id === choicesID)
+    // console.log(choiceDetails);
+    setPlayerChoice(choiceDetails[0]);    
+    
+    scoreHandler(choiceDetails[0], computerDetail);
+    let gameData = {
+      "round": rounds,
+      "player1_choice": choiceDetails[0].id,
+      "player2_choice": computerDetail.id,
+      "player1_score": 0,
+      "player2_score": 0
+   }
 
-  const generateComputerChoice = () => {
-    const randomNum = Math.floor(Math.random() * 3);
-    setComputerChoice(computerChoice);
-  };
+   
+   if((choiceDetails[0].id === 1 && computerDetail.id === 3) || (choiceDetails[0].id === 3 && computerDetail.id === 2) || (choiceDetails[0].id === 2 && computerDetail.id === 1)){
+     gameData.player1_score = 1
+    }else if(choiceDetails[0].id === computerDetail.id){
+     gameData.player1_score = 0
+     gameData.player2_score = 0
+    }else{
+     gameData.player2_score = 1;
+   }
 
-  const startGame = async () => {
-    if (currentRound >= 6) {
-      return;
+   const {data} = await axios.put(`${API}/playgame/${gameId}`, gameData);
+   console.log(data)
+   if(rounds === 6){
+    setRounds(1);
+    setUserChoice(false);
+    if(playerScore > computerScore){
+       setFinalWinner("You");
+    }else if(playerScore === computerScore){
+      setFinalWinner("Tie");
+    }else{
+      setFinalWinner("Computer");
     }
+      setReGame(true);
+    return;
+  }
+  };
 
-    const player1_choice = icons[playerChoice].name.toLowerCase();
+  const generateComputerChoice = () =>{
+    const randomNum = Math.floor(Math.random()*3)+ 1;
+    const  choiceDetails = icons.filter((data)=> data.id === randomNum)
+    setComputerChoice(choiceDetails[0])
+    return choiceDetails[0]
+  }
 
-    try {
-      const response = await axios.post(`${API}/playgame`, {
-        player1_name: "San",
-        player1_choice: player1_choice,
-        gameId,
-      });
-
-      const {
-        rounds = [],
-        player1_score,
-        player2_score,
-        final_winner,
-      } = response.data;
-
-      // Update the scores and rounds based on the response
-      setRounds((prevRounds) => [...prevRounds, ...rounds]); // Append all rounds
-      setPlayerScore(response.data.player1_score);
-      setComputerScore(response.data.computer_score);
-      if (rounds.length > 0) {
-        const lastRound = rounds[rounds.length - 1];
-        setComputerChoice(
-          icons.findIndex(
-            (icon) => icon.name.toLowerCase() === lastRound.player2_choice
-          )
-        );
+  const scoreHandler = (player1_choice, player2_choice ) => {   
+      if (player1_choice.id === player2_choice.id) {
+        return;
       }
-      // Check if the game is completed
-      if (final_winner) {
-        setFinalWinner(final_winner);
-        console.log(final_winner);
+      if (
+        (player1_choice.id === 1 && player2_choice.id === 3) ||
+        (player1_choice.id === 3 && player2_choice.id === 2) ||
+        (player1_choice.id === 2 && player2_choice.id === 1)
+      ) {
+        setPlayerScore(playerScore + 1);
+        return;
       } else {
-        setCurrentRound((prev) => prev + 1); // Only increment if the game is still ongoing
+        setComputerScore(computerScore + 1);
+        return;
       }
-    } catch (error) {
-      console.error("Error playing the game:", error);
-    }
-  };
+
+  }
+
+  const playGame = async () => {
+     setStartGame(false);
+     setUserChoice(true);
+     const {data} = await axios.post(`${API}/playgame`);
+     sessionStorage.setItem("gameId", data.game_id);
+  }
+
+  const playAgain = async () => {
+     setReGame(false);
+     setPlayerScore(0);
+     setComputerScore(0);
+     setPlayerChoice(null);
+     setComputerChoice(null);
+     setFinalWinner(null);
+     sessionStorage.clear();
+     setUserChoice(true);
+     const {data} = await axios.post(`${API}/playgame`);
+     sessionStorage.setItem("gameId", data.game_id);
+  }
+  
+
+  // const startGame = async () => {
+  //   if (currentRound >= 6) {
+  //     return;
+  //   }
+
+  //   const player1_choice = icons[playerChoice].name.toLowerCase();
+
+  //   try {
+  //     const response = await axios.post(`${API}/playgame`, {
+  //       player1_name: "San",
+  //       player1_choice: player1_choice,
+  //       gameId,
+  //     });
+
+  //     const {
+  //       rounds = [],
+  //       player1_score,
+  //       player2_score,
+  //       final_winner,
+  //     } = response.data;
+
+  //     // Update the scores and rounds based on the response
+  //     setRounds((prevRounds) => [...prevRounds, ...rounds]); // Append all rounds
+  //     setPlayerScore(response.data.player1_score);
+  //     setComputerScore(response.data.computer_score);
+  //     if (rounds.length > 0) {
+  //       const lastRound = rounds[rounds.length - 1];
+  //       setComputerChoice(
+  //         icons.findIndex(
+  //           (icon) => icon.name.toLowerCase() === lastRound.player2_choice
+  //         )
+  //       );
+  //     }
+  //     // Check if the game is completed
+  //     if (final_winner) {
+  //       setFinalWinner(final_winner);
+  //       console.log(final_winner);
+  //     } else {
+  //       setCurrentRound((prev) => prev + 1); // Only increment if the game is still ongoing
+  //     }
+  //   } catch (error) {
+  //     console.error("Error playing the game:", error);
+  //   }
+  // };
+
+  useEffect(()=>{
+    sessionStorage.clear();
+  },[])
 
   return (
     <div className="container-fluid bg-secondary-subtle vh-100 ">
@@ -98,30 +188,24 @@ const GamePage = () => {
         <div className="col-3 px-3 py-5 rounded-3 shadow bg-light ">
           <h5 className="fw-bolder ">Player 1</h5>
           <p>
-            <b>Name:</b> San
+            <b>Name:</b> You
           </p>
           <p>Score:{playerScore}</p>
         </div>
         <div className="col-6 align-self-center">
           {finalWinner ? (
-            <p className="m-0 tada">Game Over! Final Winner: {finalWinner}</p>
+            <p className="m-0 tada">Game Over !!! <br/> Final Winner: {finalWinner}</p>
           ) : (
             <div className="row d-flex justify-content-center">
               <div className="col-6">
                 {playerChoice !== null ? (
                   <>
-                    <i
-                      className={`${icons[playerChoice].iconClass} shadow p-2 rounded-4 border ${icons[playerChoice].color}`}
-                      style={{ fontSize: 70 }}
-                    ></i>
-                    <p>{icons[playerChoice].name}</p>
+                    <i className={`${playerChoice.iconClass} shadow p-2 rounded-4 border ${playerChoice.color}`} style={{ fontSize: 70 }}></i>
+                    <p>{playerChoice.name}</p>
                   </>
                 ) : (
                   <>
-                    <i
-                      className="fa-solid fa-question-circle shadow p-2 rounded-4 border text-muted"
-                      style={{ fontSize: 70 }}
-                    ></i>
+                    <i className="fa-solid fa-question-circle shadow p-2 rounded-4 border text-muted" style={{ fontSize: 70 }}></i>
                     <p className="m-0">Choose option!</p>
                   </>
                 )}
@@ -129,18 +213,12 @@ const GamePage = () => {
               <div className="col-6">
                 {computerChoice !== null ? (
                   <>
-                    <i
-                      className={`${icons[computerChoice].iconClass} shadow p-2 rounded-4 border ${icons[computerChoice].color}`}
-                      style={{ fontSize: 70 }}
-                    ></i>
-                    <p>{icons[computerChoice].name}</p>
+                    <i className={`${computerChoice.iconClass} shadow p-2 rounded-4 border ${computerChoice.color}`} style={{ fontSize: 70 }} ></i>
+                    <p>{computerChoice.name}</p>
                   </>
                 ) : (
                   <>
-                    <i
-                      className="fa-solid fa-question-circle shadow p-2 rounded-4 border text-muted"
-                      style={{ fontSize: 70 }}
-                    ></i>
+                    <i className="fa-solid fa-question-circle shadow p-2 rounded-4 border text-muted" style={{ fontSize: 70 }}></i>
                     <p className="m-0">Choose option!</p>
                   </>
                 )}
@@ -157,38 +235,22 @@ const GamePage = () => {
         </div>
       </div>
 
-      <div className="row justify-content-center m-5">
+      { userChoice &&  <div className="row justify-content-center m-5">
         <div className="d-flex mt-5 gap-1 justify-content-around w-50 ">
           {icons.map((icon, index) => (
-            <div
-              key={index}
-              className="shadow icon-container pointer text-center px-2 rounded-4 border"
-            >
-              <i
-                className={`${icon.iconClass} fs-1 mt-3 ${icon.color}`}
-                onClick={() => selectedChoice(index)}
-              ></i>
+            <div key={icon.id} onClick={() => selectedChoice(icon.id)} className="shadow icon-container pointer text-center px-2 rounded-4 border">
+              <i className={`${icon.iconClass} fs-1 mt-3 ${icon.color}`}></i>
               <p className="m-0">{icon.name}</p>
             </div>
           ))}
         </div>
-      </div>
+      </div>}
       <div>
-        <button
-          type="button"
-          className="btn btn-dark px-3"
-          onClick={startGame}
-          disabled={finalWinner !== null || currentRound >= 6}
-        >
-          {finalWinner ? "Game Over" : "Play"}
-        </button>
-        <button
-          type="button"
-          className="btn btn-dark px-3 ms-2 "
-          // onClick={resetGame}
-        >
-          Reset Game
-        </button>
+        {
+          startGame &&
+          <button type="button" className="btn btn-dark px-3" onClick={playGame}>Play</button>
+        }
+        {reGame && <button type="button"  className="btn btn-dark px-3 ms-2 " onClick={playAgain} >Play Again!</button>}
         <button
           type="button"
           className="btn btn-dark px-3 ms-2 "
